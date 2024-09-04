94d824624f63fbc2bbd782516f0b6db2f0becd1c
Đinh Thảo Vy <vydthao04@gmail.com>
2024-09-04 16:22:07 +0700

    Add files via upload

---
+++ Translating RNA to protein.py
@@ -0,0 +1,41 @@
+# Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
+# Return: The protein string encoded by s
+######################################################################################
+
+#Create a dictionary with keys = codons and values = initials
+rna_codon = {
+    'UUU': 'F',     'CUU': 'L',     'AUU': 'I',     'GUU': 'V',
+    'UUC': 'F',     'CUC': 'L',     'AUC': 'I',     'GUC': 'V',
+    'UUA': 'L',     'CUA': 'L',     'AUA': 'I',     'GUA': 'V',
+    'UUG': 'L',     'CUG': 'L',     'AUG': 'M',     'GUG': 'V',
+    'UCU': 'S',     'CCU': 'P',     'ACU': 'T',     'GCU': 'A',
+    'UCC': 'S',     'CCC': 'P',     'ACC': 'T',     'GCC': 'A',
+    'UCA': 'S',     'CCA': 'P',     'ACA': 'T',     'GCA': 'A',
+    'UCG': 'S',     'CCG': 'P',     'ACG': 'T',     'GCG': 'A',
+    'UAU': 'Y',     'CAU': 'H',     'AAU': 'N',     'GAU': 'D',
+    'UAC': 'Y',     'CAC': 'H',     'AAC': 'N',     'GAC': 'D',
+    'UAA': 'Stop',  'CAA': 'Q',     'AAA': 'K',     'GAA': 'E',
+    'UAG': 'Stop',  'CAG': 'Q',     'AAG': 'K',     'GAG': 'E',
+    'UGU': 'C',     'CGU': 'R',     'AGU': 'S',     'GGU': 'G',
+    'UGC': 'C',     'CGC': 'R',     'AGC': 'S',     'GGC': 'G',
+    'UGA': 'Stop',  'CGA': 'R',     'AGA': 'R',     'GGA': 'G',
+    'UGG': 'W',     'CGG': 'R',     'AGG': 'R',     'GGG': 'G'
+}
+
+#Read file (formated in txt)
+with open ("D:/BO/DA-08/.ipynb_checkpoints/Rosalind_practice/rosalind_prot.txt",'r') as f:
+    file = [j.strip() for j in f.readlines()]
+seq = ''.join(file)
+
+#Translate rna to protein
+list = []
+def translate_rna_to_protein(seq):
+    for i in range(0,len(seq), 3):
+        trans = rna_codon[seq[i:i+3]]
+        if trans == 'Stop':
+            break
+        list.append(trans)
+    return ''.join(list)
+
+#Print the protein sequence
+print(translate_rna_to_protein(seq))
